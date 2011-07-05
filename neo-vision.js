e9237db0eab84df1de937d7a56e87e816b06f6c3
David DeSandro <david@desandro.com>
2011-07-05 18:04:41 -0400

    add background; move back options into page; add README;

--- neo-vision.js
+++ neo-vision.js
@@ -1,24 +1,28 @@
 (function() {
 
-  console.log('execute: NEO VISION')
+  chrome.extension.sendRequest({method: "getStyle"}, function(response) {
+    console.log('extension status', response);
+    
+    var pres = document.getElementsByTagName('pre');
 
-  var pres = document.getElementsByTagName('pre');
-  
-  if ( !pres.length || document.body.querySelectorAll('*').length !== 1 ) {
-    console.log('not proceeding')
-    return;
-  }
-  
-  var style = document.createElement("style");
-  style.type = "text/css";
-  console.log( localStorage['neoVisionStyle'] )
-  style.textContent = localStorage['neoVisionStyle'];
-  document.head.appendChild(style);
-  
-      // get file extension
-  var ext = window.location.href.split('/').pop().match(/\.(js|css)/i).slice(1);
-  console.log( ext );
-  pres[0].className = "prettyprint lang-" + ext;
-  prettyPrint();
+    if ( !pres.length || document.body.querySelectorAll('*').length !== 1 ) {
+      console.log('not proceeding')
+      return;
+    }
+
+    var style = document.createElement("style");
+    style.type = "text/css";
+    console.log( response )
+    style.textContent = response;
+    document.head.appendChild(style);
+
+        // get file extension
+    var ext = window.location.href.split('/').pop().match(/\.(js|css)/i).slice(1);
+    console.log( ext );
+    pres[0].className = "prettyprint lang-" + ext;
+    prettyPrint();
+  });
+
+
 
 })();
