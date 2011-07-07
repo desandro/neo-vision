170953d0fef50412b4279c9347e27fdea55b8556
David DeSandro <david@desandro.com>
2011-07-07 07:31:51 -0400

    build.sh : prettify.min in build directory

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 BUILD_DIR='neo-vision'
 REMOVABLES=("build.sh" "prettify.js" "lang-css.js" "README.mdown")
 
-# minifies prettify js
-cat prettify.js lang-css.js | uglifyjs -nc > prettify.min.js
-
 rm -rf $BUILD_DIR/
 mkdir $BUILD_DIR
 cp * $BUILD_DIR
+
+# minifies prettify js
+cat prettify.js lang-css.js | uglifyjs -nc > $BUILD_DIR/prettify.min.js
 
 for REMOVE in "${REMOVABLES[@]}"
 do
