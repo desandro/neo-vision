8f11449969ef0fb141fbf6a60671910070d8b974
David DeSandro <david@desandro.com>
2011-07-27 15:42:10 -0400

    build.sh : copy only files; remove zip;

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 BUILD_DIR='neo-vision'
-REMOVABLES=("build.sh" "prettify.js" "lang-css.js" "README.mdown")
+ZIP='neo-vision.zip'
+REMOVABLES=("build.sh" "prettify.js" "lang-css.js" "README.mdown" $ZIP)
 
-rm neo-vision.zip
 rm -rf $BUILD_DIR/
 mkdir $BUILD_DIR
-cp * $BUILD_DIR
+cp *.* $BUILD_DIR
 
 # minifies prettify js
 cat prettify.js lang-css.js | uglifyjs -nc > $BUILD_DIR/prettify.min.js
@@ -16,4 +16,4 @@ do
 	rm $BUILD_DIR/$REMOVE
 done
 
-zip -r neo-vision.zip $BUILD_DIR
+zip -r -u $ZIP $BUILD_DIR
