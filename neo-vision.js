9c08f27fddb515151b0f2f0cea722d3aa6157eb1
David DeSandro <david@desandro.com>
2011-07-27 15:12:47 -0400

    Don't pretty print if filesize is > 70KB. re: #1

--- neo-vision.js
+++ neo-vision.js
@@ -2,22 +2,34 @@
 
   var pres = document.getElementsByTagName('pre');
 
+  // don't proceed if no <pre> or more than one item
   if ( !pres.length || document.body.querySelectorAll('*').length !== 1 ) {
     return;
   }
 
   chrome.extension.sendRequest({method: "getStyle"}, function(response) {
     
-    console.log('Neo-vision active');
-    
-    var style = document.createElement("style");
+    var pre = pres[0],
+        filesize = Math.round( pre.textContent.length / 1024 ), // in KB
+        style = document.createElement("style");
+
+    // add theme CSS
     style.textContent = response;
     document.head.appendChild(style);
+    
+    // back out if its a big file
+    // Prettyprint craps out at about 70KB and will crash the browser
+    if ( filesize > 70 ) {
+      console.log('Whoa, that\'s a big file. ' + filesize + 'KB. Neo-vision is not active so it won\'t crash this browser window.');
+      return;
+    }
 
     // get file extension
     var ext = window.location.href.split('/').pop().match(/\.(js|css)/i).slice(1);
-    pres[0].className = "prettyprint lang-" + ext;
+    pre.className = "prettyprint lang-" + ext;
     prettyPrint();
+    
+    console.log('Neo-vision active');
   });
 
 })();
